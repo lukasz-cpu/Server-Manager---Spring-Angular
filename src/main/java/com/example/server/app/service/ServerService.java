4204fd3afd1a3c2a12886aad5ded6e37e5d5da0c
lukasz_p <merc.angel@hotmail.com>
2021-10-30 23:15:32 +0200

    123

--- src/main/java/com/example/server/app/service/ServerService.java
+++ src/main/java/com/example/server/app/service/ServerService.java
@@ -2,6 +2,9 @@ package com.example.server.app.service;
 
 import com.example.server.app.model.Server;
 
+import java.util.List;
+
 public interface ServerService {
     Server create(Server server);
+    List<Server> list (int limit);
 }
