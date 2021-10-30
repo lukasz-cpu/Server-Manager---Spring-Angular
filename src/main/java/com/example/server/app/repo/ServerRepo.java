d6b1e5ee94036400fcaa28a3b8621a9c5a2b3945
lukasz_p <merc.angel@hotmail.com>
2021-10-30 22:48:53 +0200

    asd

---
+++ src/main/java/com/example/server/app/repo/ServerRepo.java
@@ -0,0 +1,13 @@
+package com.example.server.app.repo;
+
+import com.example.server.app.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepo extends JpaRepository<Server, Long> {
+
+    Server findByIpAddress(String ipAddress);
+
+
+
+
+}
