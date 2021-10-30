c442ab56b12bc9c71272893d345d5bccf220b5c2
lukasz_p <merc.angel@hotmail.com>
2021-10-30 22:00:43 +0200

    asd

---
+++ src/main/java/com/example/server/app/model/Server.java
@@ -0,0 +1,13 @@
+package com.example.server.app.model;
+
+import com.example.server.app.enumeration.Status;
+
+public class Server {
+    private Long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
