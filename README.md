cabb6122cf3051b974af0abf993db55f20d16171
lukasz_p <merc.angel@hotmail.com>
2021-10-30 21:39:51 +0200

    123

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Server Manager - Spring/Angular
- Application made while learning angular, as a part of expanding the front-end knowledge 
+
+Application made while learning angular, as a part of expanding the front-end knowledge 
---
+++ src/test/java/com/example/server/app/ServerAppTests.java
@@ -0,0 +1,13 @@
+package com.example.server.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
