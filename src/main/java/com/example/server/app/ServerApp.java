51ef4ad28dd575502572a9cda80c9a203204bc5a
lukasz_p <merc.angel@hotmail.com>
2021-11-01 13:40:15 +0100

    asd

--- src/main/java/com/example/server/app/ServerApp.java
+++ src/main/java/com/example/server/app/ServerApp.java
@@ -1,7 +1,14 @@
 package com.example.server.app;
 
+import com.example.server.app.model.Server;
+import com.example.server.app.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.example.server.app.enumeration.Status.SERVER_DOWN;
+import static com.example.server.app.enumeration.Status.SERVER_UP;
 
 @SpringBootApplication
 public class ServerApp {
@@ -9,5 +16,16 @@ public class ServerApp {
     public static void main(String[] args) {
         SpringApplication.run(ServerApp.class, args);
     }
+
+    @Bean
+    CommandLineRunner run(ServerRepo serverRepo) {
+        return args -> {
+            serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", SERVER_UP));
+            serverRepo.save(new Server(null, "192.168.1.58", "Fedora Linux", "16 GB", "Dell Tower","http://localhost:8080/server/image/server2.png", SERVER_DOWN));
+            serverRepo.save(new Server(null, "192.168.1.21", "MS 2008", "32 GB", "Web Server", "http://localhost:8080/server/image/server3.png", SERVER_UP));
+            serverRepo.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "http://localhost:8080/server/image/server4.png", SERVER_DOWN));
+        };
+    }
+
 
 }
