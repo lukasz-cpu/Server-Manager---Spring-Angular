c6960e5fa96db3deeb5179560143bf80eb3646c9
lukasz_p <merc.angel@hotmail.com>
2021-11-01 11:04:53 +0100

    asd

--- src/main/java/com/example/server/app/service/ServerService.java
+++ src/main/java/com/example/server/app/service/ServerService.java
@@ -2,10 +2,13 @@ package com.example.server.app.service;
 
 import com.example.server.app.model.Server;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.List;
 
 public interface ServerService {
     Server create(Server server);
+    Server ping(String ipAddress) throws IOException;
     List<Server> list (int limit);
     Server get(Long id);
     Server update(Server server);
--- src/main/java/com/example/server/app/service/implementation/ServerServiceImpl.java
+++ src/main/java/com/example/server/app/service/implementation/ServerServiceImpl.java
@@ -1,33 +1,75 @@
 package com.example.server.app.service.implementation;
 
+import com.example.server.app.enumeration.Status;
 import com.example.server.app.model.Server;
+import com.example.server.app.repo.ServerRepo;
 import com.example.server.app.service.ServerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.List;
 
+import static java.lang.Boolean.TRUE;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerServiceImpl implements ServerService {
+
+    private final ServerRepo serverRepo;
+
+
     @Override
     public Server create(Server server) {
-        return null;
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
+    }
+
+
+    @Override
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(1000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        return serverRepo.save(server);
     }
 
     @Override
     public List<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepo.findAll(PageRequest.of(0, limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by id: {}", id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server: {}", server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
+        log.info("Deleting server by ID: {}", id);
+        serverRepo.deleteById(id);
+        return TRUE;
+
+    }
+
+    private String setServerImageUrl() {
+        String[] imageNames = {"server1.png", "server2.png", "server3.png", "server4.png"};
         return null;
     }
 }
