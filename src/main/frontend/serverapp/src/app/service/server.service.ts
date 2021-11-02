fcde8bb271c95b7bdc2af6435d411799990f58a6
lukasz_p <merc.angel@hotmail.com>
2021-11-02 18:02:58 +0100

    asd

--- src/main/frontend/serverapp/src/app/service/server.service.ts
+++ src/main/frontend/serverapp/src/app/service/server.service.ts
@@ -1,9 +1,21 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {CustomResponse} from "../interface/custom-response";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
 //1:22
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+
+
+  getServers(): Observable<CustomResponse>{
+    return this.http.get<CustomResponse>('http://localhost:8080/server/list')
+  }
+
+
 }
